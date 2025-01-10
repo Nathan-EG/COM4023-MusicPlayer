a2fe36e99d6ed919c7453235b4d6f4c070ee574b
Nathan-EG <nellis-greenwood@burnley.ac.uk>
2025-01-10 09:39:26 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,26 +7,26 @@ public class Main {
 
         //Add Songs
         Song[] songs = new Song[10];
-        songs[0]=new Song("G", "In the Beginning",1969);
-        songs[1]=new Song("Genesis", "Where the Sour Turns to Sweet",1969);
-        songs[2]=new Song("Genesis", "The Silent Sun",1969);
-        songs[3]=new Song("Genesis", "Robbery, Assault and Battery",1973);
-        songs[4]=new Song("Genesis", "A Trick of the Tail",1973);
-        songs[5]=new Song("Genesis", "Ripples",1973);
-        songs[6]=new Song("Genesis", "Firth of Fifth",1973);
-        songs[7]=new Song("Genesis", "Dancing with the Moonlit Knight",1973);
-        songs[8]=new Song("Genesis", "The Carpet Crawlers",1974);
-        songs[9]=new Song("Genesis", "In the Cage",1974);
+        songs[0]=new Song("O", "Morning Glory?",1995);
+        songs[1]=new Song("Oasis", "Hello",1995);
+        songs[2]=new Song("Oasis", "Roll With It",1995);
+        songs[3]=new Song("Oasis", "Wonderwall",1995);
+        songs[4]=new Song("Oasis", "Don't Look Back In Anger",1995);
+        songs[5]=new Song("Oasis", "Hey Now!",1995);
+        songs[6]=new Song("Oasis", "The Swamp Song",1995);
+        songs[7]=new Song("Oasis", "Some Might Say",1995);
+        songs[8]=new Song("Oasis", "Cast No Shadow",1995);
+        songs[9]=new Song("Oasis", "She's Electric",1995);
 
         for (Song song:songs){
             musicList.addSong(song);
         }
         musicList.printMusicList();
         System.out.println(musicList.getMusicListSize());
-        musicList.findByArtist("Genesis");
+        musicList.findByArtist("Oasis");
         musicList.getRandomSong();
-        musicList.removeSong("Firth of Fifth");
-        System.out.println("==================");
+        musicList.removeSong("The Swamp Song");
+        System.out.println("----------------");
         musicList.printMusicList();
     }
 }
